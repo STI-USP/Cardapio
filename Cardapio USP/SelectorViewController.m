088812b6a6284a40bcdca370d09ba0674b170ec8
Vagner Machado <vmachado@usp.br>
2014-05-27 16:43:20 -0300

    Lista de restaurantes com expand/collapse

--- Cardapio USP/SelectorViewController.m
+++ Cardapio USP/SelectorViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "SelectorViewController.h"
+#import "MainViewController.h"
 
 @interface SelectorViewController () {
-  NSArray *restauramtList;
+  NSMutableArray *restaurantList;
+  NSMutableDictionary *sectionContentDict;
+  NSMutableArray      *arrayForBool;
+
 }
 
 @end
@@ -28,7 +32,31 @@
 - (void)viewDidLoad{
   [super viewDidLoad];
   
-  restauramtList = @[@"CUASO", @"   Central", @"   Física", @"   Químicas", @"   PUSP-C", @"EACH", @"..."];
+    if (!restaurantList) {
+        restaurantList = [NSMutableArray arrayWithObjects:@"CUASO", @"EACH", @"SÃO FRANCISCO", @"SAÚDE", @"LORENA", nil];
+    }
+    if (!arrayForBool) {
+        arrayForBool    = [NSMutableArray arrayWithObjects:[NSNumber numberWithBool:NO],
+                           [NSNumber numberWithBool:NO],
+                           [NSNumber numberWithBool:NO],
+                           [NSNumber numberWithBool:NO],
+                           [NSNumber numberWithBool:NO] , nil];
+    }
+    if (!sectionContentDict) {
+        sectionContentDict  = [[NSMutableDictionary alloc] init];
+        NSArray *array1     = [NSArray arrayWithObjects:@"Central", @"Física", @"Químicas", @"PUSP-C", nil];
+        [sectionContentDict setValue:array1 forKey:[restaurantList objectAtIndex:0]];
+        NSArray *array2     = [NSArray arrayWithObjects:@" ", nil];
+        [sectionContentDict setValue:array2 forKey:[restaurantList objectAtIndex:1]];
+        NSArray *array3     = [NSArray arrayWithObjects:@" ", nil];
+        [sectionContentDict setValue:array3 forKey:[restaurantList objectAtIndex:2]];
+        NSArray *array4     = [NSArray arrayWithObjects:@" ", nil];
+        [sectionContentDict setValue:array4 forKey:[restaurantList objectAtIndex:3]];
+        NSArray *array5     = [NSArray arrayWithObjects:@" ", nil];
+        [sectionContentDict setValue:array5 forKey:[restaurantList objectAtIndex:4]];
+    }
+
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,23 +68,97 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-  return 1;
+    return [restaurantList count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  return restauramtList.count;
+    if ([[arrayForBool objectAtIndex:section] boolValue]) {
+        return [[sectionContentDict valueForKey:[restaurantList objectAtIndex:section]] count];
+    }
+    return 1;
 }
 
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UIView *headerView              = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 50)];
+    headerView.tag                  = section;
+    headerView.backgroundColor      = [UIColor whiteColor];
+    UILabel *headerString           = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, self.view.frame.size.width-20-50, 50)];
+    BOOL manyCells                  = [[arrayForBool objectAtIndex:section] boolValue];
+
+    headerString.text = [restaurantList objectAtIndex:section];
+
+    if (!manyCells) {
+        headerString.textColor = [UIColor blackColor];
+    }else{
+        headerString.textColor = [UIColor redColor];
+    }
+    headerString.textAlignment      = NSTextAlignmentLeft;
+    [headerView addSubview:headerString];
+    
+    UITapGestureRecognizer  *headerTapped   = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sectionHeaderTapped:)];
+    [headerView addGestureRecognizer:headerTapped];
+    
+    return headerView;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section{
+    UIView *footer  = [[UIView alloc] initWithFrame:CGRectZero];
+    return footer;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 42;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    return 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if ([[arrayForBool objectAtIndex:indexPath.section] boolValue]) {
+        return 42;
+    }
+    return 0;
+}
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RestaurantCell" forIndexPath:indexPath];
-    cell.textLabel.text = restauramtList[indexPath.row];
-  if (indexPath.row == 1) {
-    cell.accessoryType = UITableViewCellAccessoryCheckmark;
-  }
+    //cell.textLabel.text = restaurantList[indexPath.row];
+  //if (indexPath.row == 1) {
+  //  cell.accessoryType = UITableViewCellAccessoryCheckmark;
+  //}
+    
+    BOOL manyCells  = [[arrayForBool objectAtIndex:indexPath.section] boolValue];
+    if (!manyCells) {
+        cell.textLabel.text = [restaurantList objectAtIndex:indexPath.section];
+    }
+    else{
+        NSArray *content = [sectionContentDict valueForKey:[restaurantList objectAtIndex:indexPath.section]];
+        cell.textLabel.text = [content objectAtIndex:indexPath.row];
+    }
+
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+}
+
+#pragma mark - gesture tapped
+- (void)sectionHeaderTapped:(UITapGestureRecognizer *)gestureRecognizer{
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:gestureRecognizer.view.tag];
+    if (indexPath.row == 0) {
+        BOOL collapsed = [[arrayForBool objectAtIndex:indexPath.section] boolValue];
+        collapsed = !collapsed;
+        [arrayForBool replaceObjectAtIndex:indexPath.section withObject:[NSNumber numberWithBool:collapsed]];
+        
+        //reload specific section animated
+        NSRange range   = NSMakeRange(indexPath.section, 1);
+        NSIndexSet *sectionToReload = [NSIndexSet indexSetWithIndexesInRange:range];
+        [self.tableView reloadSections:sectionToReload withRowAnimation:UITableViewRowAnimationFade];
+    }
+}
 
 /*
 // Override to support conditional editing of the table view.
