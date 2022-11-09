ba116ad0e0dcbf54721a9bf930e5f841973d8436
Vagner Machado <vmachado@usp.br>
2022-11-09 17:44:06 -0300

    retira validaçao do valor minimo para pix

--- CreditsViewController.m
+++ CreditsViewController.m
@@ -142,7 +142,7 @@
 }
 
 - (IBAction)gerarPix:(id)sender {
-  if ([self validarValorRecarga:20]) {
+  if ([self validarValorRecarga:0]) {
     [SVProgressHUD show];
     [boletoDataModel createPix];
   }
