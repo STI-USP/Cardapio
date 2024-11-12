9accee63d8045cc5dfff97f6b2462a525997879c
Vagner Machado <vmachado@usp.br>
2024-11-12 12:26:02 -0300

    Fix na consulta de saldo do RUCard

--- Cardapio USP/DataAccess.m
+++ Cardapio USP/DataAccess.m
@@ -48,16 +48,16 @@
 - (void)consultarSaldo {
     // Verifica se oauth e userData não são nil
     if (oauth == nil || oauth.userData == nil) {
-        NSLog(@"Erro: oauth ou oauth.userData é nil.");
-        [SVProgressHUD showErrorWithStatus:@"Erro de autenticação. Por favor, faça login novamente."];
+        NSLog(@"oauth ou oauth.userData é nil.");
+        [SVProgressHUD showErrorWithStatus:@"Não foi possível verificar o usuário. Por favor, faça login novamente."];
         return;
     }
 
     // Recupera wsuserid
     NSString *wsuserid = [oauth.userData valueForKey:@"wsuserid"];
     if (wsuserid == nil) {
-        NSLog(@"Erro: wsuserid é nil.");
-        [SVProgressHUD showErrorWithStatus:@"Erro ao recuperar o identificador do usuário."];
+        NSLog(@"wsuserid é nil.");
+      [SVProgressHUD showErrorWithStatus:@"Não foi possível identificar o usuário. Por favor, faça login novamente."];
         return;
     }
 
@@ -70,8 +70,8 @@
     NSData *params = [NSJSONSerialization dataWithJSONObject:parameters options:0 error:&error];
     
     if (error) {
-        NSLog(@"Erro ao serializar parâmetros: %@", error.localizedDescription);
-        [SVProgressHUD showErrorWithStatus:@"Erro ao preparar os dados de solicitação."];
+        NSLog(@"Falha ao serializar parâmetros: %@", error.localizedDescription);
+        [SVProgressHUD showErrorWithStatus:@"Não foi possível preparar os dados de solicitação. Tente novamente mais tarde."];
         return;
     }
     
@@ -84,7 +84,63 @@
     [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     [urlRequest setHTTPBody:params];
     
-    // O restante do código permanece o mesmo...
+    // Cria a sessão e executa a requisição
+    NSURLSessionDataTask *dataTask = [[self session] dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
+            NSLog(@"Falha na requisição: %@", error.localizedDescription);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [SVProgressHUD showErrorWithStatus:error.localizedDescription];
+                [self->_dataModel setRuCardCredit:@"--,--"];
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"DidReceiveCredits" object:self];
+            });
+            return;
+        }
+        
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+        if (httpResponse.statusCode == 200 && data.length > 0) {
+            NSError *jsonError;
+            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonError];
+            
+            if (jsonError) {
+                NSLog(@"Falha ao processar JSON: %@", jsonError.localizedDescription);
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [SVProgressHUD showErrorWithStatus:@"Não foi possível processar a resposta do servidor. Tente novamente mais tarde."];
+                    [self->_dataModel setRuCardCredit:@"--,--"];
+                    [[NSNotificationCenter defaultCenter] postNotificationName:@"DidReceiveCredits" object:self];
+                });
+                return;
+            }
+            
+            // Processa a resposta JSON
+            if ([[json valueForKey:@"erro"] boolValue]) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self->_dataModel setRuCardCredit:@"--,--"];
+                    [SVProgressHUD showErrorWithStatus:[json valueForKey:@"mensagemErro"]];
+                    
+                    if ([[json valueForKey:@"mensagemErro"] isEqualToString:@"Usuário não está logado!"]) {
+                        [[NSNotificationCenter defaultCenter] postNotificationName:@"DidReceiveLoginError" object:self];
+                    }
+                    
+                    [[NSNotificationCenter defaultCenter] postNotificationName:@"DidReceiveCredits" object:self];
+                });
+            } else {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self->_dataModel setRuCardCredit:[json valueForKey:@"saldo"]];
+                    [SVProgressHUD dismiss];
+                    [[NSNotificationCenter defaultCenter] postNotificationName:@"DidReceiveCredits" object:self];
+                });
+            }
+        } else {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [SVProgressHUD showErrorWithStatus:@"Não foi possível obter o saldo. Tente novamente mais tarde."];
+                [self->_dataModel setRuCardCredit:@"--,--"];
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"DidReceiveCredits" object:self];
+            });
+        }
+    }];
+    
+    // Inicia a tarefa
+    [dataTask resume];
 }
 
 - (void)createPix {
