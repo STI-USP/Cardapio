f5641c98b8321b779d1dbf8d84bdb51c47f52956
Vagner Machado <vmachado@usp.br>
2025-06-13 17:21:19 -0300

    Adiciona indicador de carregamento com SVProgressHUD no WebViewViewController

--- Cardapio USP/Features/WebViewViewController.swift
+++ Cardapio USP/Features/WebViewViewController.swift
@@ -7,20 +7,39 @@
 
 import UIKit
 import WebKit
+import SVProgressHUD
 
-class WebViewViewController: UIViewController {
+class WebViewViewController: UIViewController, WKNavigationDelegate {
   var urlString: String?
+  private var webView: WKWebView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
     
-    let webView = WKWebView(frame: view.bounds)
+    webView = WKWebView(frame: view.bounds)
+    webView.navigationDelegate = self
     webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     view.addSubview(webView)
     
     if let urlString = urlString, let url = URL(string: urlString) {
-      webView.load(URLRequest(url: url))
+      let request = URLRequest(url: url)
+      SVProgressHUD.show()
+      webView.load(request)
     }
+  }
+  
+  // MARK: - WKNavigationDelegate
+  
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    SVProgressHUD.dismiss()
+  }
+  
+  func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+    SVProgressHUD.dismiss()
+  }
+  
+  func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+    SVProgressHUD.dismiss()
   }
 }
